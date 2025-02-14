692750d2d55ef83daa898b5d86fca457ba5d282f
Vishal khandelwal <129240730+vk2420@users.noreply.github.com>
2025-02-14 14:17:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Sona'; // Replace with your secret code
+const secretCode = 'Darling'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
@@ -13,6 +13,6 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri sona';
+		resultDiv.innerHTML = 'Naa tum meri darling nahi kaun ho tum';
 	}
 });
